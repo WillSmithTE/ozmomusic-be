be4f976ab0a9f17fb4afd34ef11fec3a27195a16
Will Smith <willsmithte@gmail.com>
2022-01-04 15:21:08 +0100

    download working

--- DataService.py
+++ DataService.py
@@ -7,7 +7,10 @@ import json
 import os
 from datetime import datetime
 import youtube_dl
-
+from contextlib import redirect_stdout
+from io import BytesIO
+import logging
+import uuid
 
 class DataService:
 
@@ -20,3 +23,15 @@ class DataService:
             parsedUrl,
             download=False
         )
+
+    def downloadFile(self, url):
+        parsedUrl = UrlParser.soundcloud(url)
+        
+        filename = str(uuid.uuid4())
+
+        options = {'outtmpl': filename}
+        with youtube_dl.YoutubeDL(options) as ydl:
+            metadata = ydl.extract_info(parsedUrl)
+
+        logging.debug("getFile (metadata=%s)", metadata)
+        return filename, metadata
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
 import json
 import os
 from DataService import DataService
 import logging
+import mimetypes
+import io
 
 logging.basicConfig()
-logging.root.setLevel(logging.INFO)
+logging.root.setLevel(logging.DEBUG)
 
 app = Flask(__name__)
 CORS(app)
 
 dataService = DataService()
+
 
 @app.route("/ping", methods=["GET"])
 def ping():
@@ -24,20 +27,34 @@ def getMetadata():
     logging.info("getMetadata (url=%s)", url)
     return jsonify(dataService.getMetadata(url))
 
+
 @app.route("/api/media/download", methods=["GET"])
 def getFile():
     url = request.args.get('url')
+    logging.info("getFile (url=%s)", url)
 
-    message = None
-    outdatedPlaceServices = dataService.getOutdatedPlaceServices()
-    outdatedPlaces = list(map(lambda service : service.NAME, outdatedPlaceServices))
-    if len(outdatedPlaceServices) > 0:
-        dataService.updateData(outdatedPlaceServices)
-        message = "Data has been refreshed for " + str(outdatedPlaces)
-    else:
-        message = "No new data found to refresh"
-    logging.info(message)
-    return json.dumps({"outdatedPlaces": outdatedPlaces}), 200, {"ContentType": "application/json"}
+    filePath, metadata = dataService.downloadFile(url)
+
+    data = io.BytesIO()
+    with open(filePath, 'rb') as fo:
+        data.write(fo.read())
+
+    # (after writing, cursor will be at last byte, so move it to start)
+    data.seek(0)
+
+    os.remove(filePath)
+
+    extension = metadata.get("ext")
+    mimetype = mimetypes.guess_extension(extension)
+    extractor = metadata.get("extractor")
+    songId = metadata.get("id")
+
+    return send_file(
+        data,
+        as_attachment=True,
+        download_name=extractor + "_" + songId + "." + extension,
+        mimetype=mimetype
+    )
 
 
 app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
